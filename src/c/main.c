70cbd01bedce83a56a018bb83afbd4828a0d7aa7
Tom Smalara <tsmalara@gmail.com>
2016-12-07 13:15:36 -0800

    v2 Digital First Commit

--- src/c/main.c
+++ src/c/main.c
@@ -2,17 +2,16 @@
 #include "main.h" // Used for drawing hands
 #include "gcolor_definitions.h" // Allows the use of colors such as "GColorMidnightGreen"
 
-
-
 static Window *s_main_window; 
-static Layer *s_solid_bg_layer, *s_hands_layer;
+static Layer *s_solid_bg_layer;
 static BitmapLayer *s_background_layer; 
 static GBitmap *s_background_bitmap; 
-static GPath *s_minute_arrow, *s_hour_arrow, *s_second_arrow; 
-static TextLayer *s_date_layer, *s_month_layer,*s_weather_layer, *s_temperature_layer, *s_step_layer;
+//static GPath *s_minute_arrow, *s_hour_arrow, *s_second_arrow; 
+static TextLayer *s_h1_layer, *s_h2_layer, *s_m1_layer, *s_m2_layer, *s_date_layer, *s_month_layer,*s_weather_layer, *s_temperature_layer, *s_step_layer;
 int cx, cy, xDate, yDate, xStep, yStep, xMonth, yMonth, xWeather, yWeather;
 GPoint center;
 
+//static char s_current_time_buffer[8], s_current_steps_buffer[16];
 static char s_current_steps_buffer[16];
 static int s_step_count = 0, s_step_goal = 0, s_step_average = 0;
 
@@ -27,32 +26,37 @@ static void bg_update_proc(Layer *layer, GContext *ctx) {
   cy = bounds.size.h / 2;
   center = GPoint(cx, cy);
       
+  //Center Circle
+  graphics_context_set_stroke_width(ctx, 2); 
+  graphics_context_set_stroke_color(ctx, GColorDarkGray);
+  graphics_draw_circle(ctx, GPoint(cx, cy), PBL_IF_ROUND_ELSE(65,60));
+  
   //Month Background
-  xMonth = PBL_IF_ROUND_ELSE(cx - 47, cx - 47);
-  yMonth = PBL_IF_ROUND_ELSE(45, 32);
-  graphics_context_set_fill_color(ctx, GColorRed); 
-  graphics_fill_circle(ctx, GPoint(xMonth, yMonth), 20);
+  xMonth = PBL_IF_ROUND_ELSE(cx - 47, cx - 45);
+  yMonth = PBL_IF_ROUND_ELSE(45, cy - 40);
+  graphics_context_set_fill_color(ctx, GColorIslamicGreen); 
+  graphics_fill_circle(ctx, GPoint(xMonth, yMonth), 18);
   
   //Day Background
-  xDate = PBL_IF_ROUND_ELSE(cx + 47, cx + 47);
-  yDate = PBL_IF_ROUND_ELSE(45, 32);
-  graphics_context_set_fill_color(ctx, GColorIslamicGreen); 
-  graphics_fill_circle(ctx, GPoint(xDate, yDate), 20);
+  xDate = PBL_IF_ROUND_ELSE(cx + 47, cx + 45);
+  yDate = PBL_IF_ROUND_ELSE(45, cy - 40);
+  graphics_context_set_fill_color(ctx, GColorOrange); 
+  graphics_fill_circle(ctx, GPoint(xDate, yDate), 18);
   
   //Step Background
-  xStep = PBL_IF_ROUND_ELSE(cx + 47, cx + 47);
-  yStep = PBL_IF_ROUND_ELSE(135, 140);
+  xStep = PBL_IF_ROUND_ELSE(cx + 47, cx + 45);
+  yStep = PBL_IF_ROUND_ELSE(135, cy + 40);
   graphics_context_set_fill_color(ctx, GColorFashionMagenta); 
-  graphics_fill_circle(ctx, GPoint(xStep, yStep), 20);
+  graphics_fill_circle(ctx, GPoint(xStep, yStep), 18);
   
   //Weather Background
-  xWeather = PBL_IF_ROUND_ELSE(cx - 47, cx - 47);
-  yWeather = PBL_IF_ROUND_ELSE(135, 140);
+  xWeather = PBL_IF_ROUND_ELSE(cx - 47, cx - 45);
+  yWeather = PBL_IF_ROUND_ELSE(135, cy + 40);
   graphics_context_set_fill_color(ctx, GColorBlueMoon); 
-  graphics_fill_circle(ctx, GPoint(xWeather, yWeather), 20);
+  graphics_fill_circle(ctx, GPoint(xWeather, yWeather), 18);
   
   //dot top center
-  graphics_context_set_fill_color(ctx, GColorOrange); 
+  graphics_context_set_fill_color(ctx, GColorYellow); 
   graphics_fill_circle(ctx, GPoint(cx, PBL_IF_ROUND_ELSE(25,23)), 14);
   
   //dot bottom center
@@ -60,12 +64,13 @@ static void bg_update_proc(Layer *layer, GContext *ctx) {
   graphics_fill_circle(ctx, GPoint(cx, PBL_IF_ROUND_ELSE(155, 145)), 14);
    
   //dot left
-  graphics_context_set_fill_color(ctx, GColorCyan); 
-  graphics_fill_circle(ctx, GPoint(PBL_IF_ROUND_ELSE(25,15), cy), 14);
+  graphics_context_set_fill_color(ctx, GColorTiffanyBlue ); 
+  graphics_fill_circle(ctx, GPoint(PBL_IF_ROUND_ELSE(25,cx - 60), cy), 10);
   
   //dot right
-  graphics_context_set_fill_color(ctx, GColorYellow); 
-  graphics_fill_circle(ctx, GPoint(PBL_IF_ROUND_ELSE(155, 129), cy), 14);
+  graphics_context_set_fill_color(ctx, GColorRed); 
+  graphics_fill_circle(ctx, GPoint(PBL_IF_ROUND_ELSE(155, cx + 60), cy), 10);
+  
 }
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
@@ -103,7 +108,6 @@ static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResul
 static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
-
 
 // Health Data Start
 bool step_data_is_available() {
@@ -162,61 +166,42 @@ static void health_handler(HealthEventType event, void *context) {
 }
 // Health Data End
 
-static void hands_update_proc(Layer *layer, GContext *ctx) {
-  time_t now = time(NULL); 
-  struct tm *t = localtime(&now); 
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
   
+  static char h_buffer[8];
+  strftime(h_buffer, sizeof("00"), "%I", tick_time);
+  static char m_buffer[8];
+  strftime(m_buffer, sizeof("00"), "%M", tick_time);
+
+  // Display this time on the TextLayer
+  static char h1[] = " ";
+  strncpy(h1, &h_buffer[0], 1);
+  text_layer_set_text(s_h1_layer, h1);
+  
+  text_layer_set_text(s_h2_layer, &h_buffer[1]);
+  
+  static char m1[] = " ";
+  strncpy(m1, &m_buffer[0], 1);
+  text_layer_set_text(s_m1_layer, m1);
+ 
+  text_layer_set_text(s_m2_layer, &m_buffer[1]);
+    
+  // Write the current date/month into a buffer
   static char s_buffer_date[8]; 
-  strftime(s_buffer_date, sizeof("dd/MMM/yy"), "%e", t);
+  strftime(s_buffer_date, sizeof("dd/MMM/yy"), "%e", tick_time);
   text_layer_set_text(s_date_layer, s_buffer_date);
   
   static char s_buffer_month[3];
-  strftime(s_buffer_month, sizeof("dd/MMM/yy"), "%a", t); 
+  strftime(s_buffer_month, sizeof("dd/MMM/yy"), "%a", tick_time); 
   text_layer_set_text(s_month_layer, s_buffer_month);
-    
-  // Clock Background Circle
-  //graphics_context_set_fill_color(ctx, GColorWhite);  
-  //graphics_fill_circle(ctx, center, 40);
-  graphics_context_set_stroke_width(ctx, 6); 
-  graphics_context_set_stroke_color(ctx, GColorDukeBlue); 
-  graphics_draw_circle(ctx, GPoint(cx, cy), 13); 
-  graphics_context_set_stroke_color(ctx, GColorBlueMoon);
-  graphics_draw_circle(ctx, GPoint(cx, cy), 25);  
-  graphics_context_set_stroke_color(ctx, GColorPictonBlue);
-  graphics_draw_circle(ctx, GPoint(cx, cy), 38);
-  
-  // Set stroke width/color for hands
-  graphics_context_set_stroke_color(ctx, GColorBlack);
-  graphics_context_set_stroke_width(ctx, 1);
-  
-  // Minute hand
-  graphics_context_set_fill_color(ctx, GColorWhite); 
-  gpath_rotate_to(s_minute_arrow, TRIG_MAX_ANGLE * t->tm_min / 60);
-  gpath_draw_filled(ctx, s_minute_arrow); 
-  gpath_draw_outline(ctx, s_minute_arrow);
-  
-  // Hour hand
-  graphics_context_set_fill_color(ctx, GColorWhite); 
-  gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6)); 
-  gpath_draw_filled(ctx, s_hour_arrow);
-  gpath_draw_outline(ctx, s_hour_arrow);  
-  
-  //Second hand
-  //graphics_context_set_fill_color(ctx, GColorRed); 
-  //gpath_rotate_to(s_second_arrow, (TRIG_MAX_ANGLE * t->tm_sec / 60)); 
-  //gpath_draw_filled(ctx, s_second_arrow);  
-  
-  // Center dot  
-  graphics_context_set_fill_color(ctx, GColorRed); 
-  graphics_fill_circle(ctx, center, 5);
-  graphics_context_set_stroke_color(ctx, GColorBlack);
-  graphics_context_set_stroke_width(ctx, 1);
-  graphics_draw_circle(ctx, GPoint(cx, cy), 5);
-  
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  layer_mark_dirty(window_get_root_layer(s_main_window));
+  update_time();
   
   // Get weather update every 30 minutes
   if(tick_time->tm_min % 30 == 0) {
@@ -235,14 +220,13 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void window_load(Window *s_main_window) {
   Layer *window_layer = window_get_root_layer(s_main_window); 
   GRect bounds = layer_get_bounds(window_layer); 
-
+  cx = bounds.size.w / 2;
+  cy = bounds.size.h / 2;
   s_solid_bg_layer = layer_create(bounds); 
   s_background_layer = bitmap_layer_create(bounds); 
-  s_hands_layer = layer_create(bounds); 
   
   layer_set_update_proc(s_solid_bg_layer, bg_update_proc); 
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap); 
-  layer_set_update_proc(s_hands_layer, hands_update_proc); 
   
   #if PBL_PLATFORM_BASALT // Only set this for SDK 3.0 +
     bitmap_layer_set_compositing_mode(s_background_layer, GCompOpSet); // Set background layer to be transparent
@@ -250,10 +234,9 @@ static void window_load(Window *s_main_window) {
   
   layer_add_child(window_layer, s_solid_bg_layer); 
   layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer)); 
-  layer_add_child(window_layer, s_hands_layer); 
   
    // Create MonthLayer
-  s_month_layer = text_layer_create(GRect(PBL_IF_ROUND_ELSE(-47, -47), PBL_IF_ROUND_ELSE(30, 17), bounds.size.w, 50));
+  s_month_layer = text_layer_create(GRect(PBL_IF_ROUND_ELSE(-47, -45), PBL_IF_ROUND_ELSE(30, 28), bounds.size.w, 50));
   text_layer_set_background_color(s_month_layer, GColorClear);
   text_layer_set_text_color(s_month_layer, GColorWhite);  
   text_layer_set_font(s_month_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
@@ -262,7 +245,7 @@ static void window_load(Window *s_main_window) {
   
   
   // Create DateLayer
-  s_date_layer = text_layer_create(GRect(PBL_IF_ROUND_ELSE(45, 46), PBL_IF_ROUND_ELSE(30, 17), bounds.size.w, 50));
+  s_date_layer = text_layer_create(GRect(PBL_IF_ROUND_ELSE(45, 45), PBL_IF_ROUND_ELSE(30, 28), bounds.size.w, 50));
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorWhite);  
   text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
@@ -270,7 +253,7 @@ static void window_load(Window *s_main_window) {
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
   
    // Create a layer to hold the current step count
-  s_step_layer = text_layer_create(GRect(PBL_IF_ROUND_ELSE(47,47), PBL_IF_ROUND_ELSE(120, 125), bounds.size.w, 50));
+  s_step_layer = text_layer_create(GRect(PBL_IF_ROUND_ELSE(47,45), PBL_IF_ROUND_ELSE(120, 108), bounds.size.w, 50));
   text_layer_set_background_color(s_step_layer, GColorClear);
   text_layer_set_text_color(s_step_layer, GColorWhite);
   text_layer_set_font(s_step_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
@@ -283,20 +266,71 @@ static void window_load(Window *s_main_window) {
   }
   
   // Create temperature Layer
-  s_temperature_layer = text_layer_create(GRect(PBL_IF_ROUND_ELSE(-44, -44), PBL_IF_ROUND_ELSE(120, 125), bounds.size.w, 25));
+  s_temperature_layer = text_layer_create(GRect(PBL_IF_ROUND_ELSE(-44, -42), PBL_IF_ROUND_ELSE(120, 108), bounds.size.w, 25));
   text_layer_set_background_color(s_temperature_layer, GColorClear);
   text_layer_set_text_color(s_temperature_layer, GColorWhite);
   text_layer_set_font(s_temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   text_layer_set_text_alignment(s_temperature_layer, GTextAlignmentCenter);
   text_layer_set_text(s_temperature_layer, "...");
   layer_add_child(window_layer, text_layer_get_layer(s_temperature_layer));
+  
+  // Hour 1 Layer
+  s_h1_layer = text_layer_create(GRect(-12, (bounds.size.h / 2) - 45 , bounds.size.w, bounds.size.h));
+
+  text_layer_set_background_color(s_h1_layer, GColorClear);
+  text_layer_set_text_color(s_h1_layer, GColorWhite);
+  text_layer_set_text(s_h1_layer, "00:00");
+  text_layer_set_font(s_h1_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
+  text_layer_set_text_alignment(s_h1_layer, GTextAlignmentCenter);
+  
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_h1_layer));
+  
+  // Hour 2 Layer
+  s_h2_layer = text_layer_create(GRect(12, (bounds.size.h / 2) - 45, bounds.size.w, bounds.size.h));
+
+  text_layer_set_background_color(s_h2_layer, GColorClear);
+  text_layer_set_text_color(s_h2_layer, GColorWhite);
+  text_layer_set_text(s_h2_layer, "00:00");
+  text_layer_set_font(s_h2_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
+  text_layer_set_text_alignment(s_h2_layer, GTextAlignmentCenter);
+  
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_h2_layer));
+  
+   // Minute 1 Layer
+  s_m1_layer = text_layer_create(GRect(-12, (bounds.size.h / 2) - 7 , bounds.size.w, bounds.size.h));
+
+  text_layer_set_background_color(s_m1_layer, GColorClear);
+  text_layer_set_text_color(s_m1_layer, GColorRed);
+  text_layer_set_text(s_m1_layer, "00:00");
+  text_layer_set_font(s_m1_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_m1_layer, GTextAlignmentCenter);
+  
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_m1_layer));
+  
+  // Minute 2 Layer
+  s_m2_layer = text_layer_create(GRect(12, (bounds.size.h / 2) - 7, bounds.size.w, bounds.size.h));
+
+  text_layer_set_background_color(s_m2_layer, GColorClear);
+  text_layer_set_text_color(s_m2_layer, GColorRed);
+  text_layer_set_text(s_m2_layer, "00:00");
+  text_layer_set_font(s_m2_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_m2_layer, GTextAlignmentCenter);
+  
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_m2_layer));
 }
 
 static void window_unload(Window *s_main_window) {
   layer_destroy(s_solid_bg_layer); 
   gbitmap_destroy(s_background_bitmap); 
   bitmap_layer_destroy(s_background_layer);
-  layer_destroy(s_hands_layer); 
+  text_layer_destroy(s_h1_layer); 
+  text_layer_destroy(s_h2_layer); 
+  text_layer_destroy(s_m1_layer); 
+  text_layer_destroy(s_m2_layer); 
   text_layer_destroy(s_date_layer);
   text_layer_destroy(s_month_layer);
   text_layer_destroy(s_step_layer);
@@ -311,18 +345,11 @@ static void init() {
   });
   window_stack_push(s_main_window, true); 
 
-  s_minute_arrow = gpath_create(&MINUTE_HAND_POINTS); 
-  s_hour_arrow = gpath_create(&HOUR_HAND_POINTS); 
-  s_second_arrow = gpath_create(&SEC_POINTS); 
+  // Make sure the time is displayed from the start
+  update_time();
 
-  Layer *window_layer = window_get_root_layer(s_main_window); 
-  GRect bounds = layer_get_bounds(window_layer); 
-  GPoint center = grect_center_point(&bounds); 
-  gpath_move_to(s_minute_arrow, center); 
-  gpath_move_to(s_hour_arrow, center); 
-  gpath_move_to(s_second_arrow, center); 
-
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler); 
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   
   // Register callbacks
   app_message_register_inbox_received(inbox_received_callback);
@@ -335,10 +362,6 @@ static void init() {
 }
 
 static void deinit() {
-  
-  gpath_destroy(s_minute_arrow);
-  gpath_destroy(s_hour_arrow); 
-  gpath_destroy(s_second_arrow); 
   tick_timer_service_unsubscribe(); 
   window_destroy(s_main_window); 
 }
